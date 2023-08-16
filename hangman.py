bc9922454c1a0221941a8943f940a6705365d8a7
Tobias Lust <tobiaslust.dev@gmail.com>
2023-08-15 21:23:06 -0300

    Create func start game

--- hangman.py
+++ hangman.py
@@ -7,8 +7,8 @@ from time import sleep as ts
 def main():
     # Lives
     LIVES = 6
-    BODY = ['Pierna der', 'Pierna izq', 'Brazo der',
-            'Brazo izq', 'Cuerpo', 'Cabeza']
+    PART_BODY = ['Pierna der', 'Pierna izq', 'Brazo der',
+                 'Brazo izq', 'Cuerpo', 'Cabeza']
 
     # Define words for levels
     EASY = ['gato', 'nube', 'tierra', 'arbol', 'silla', 'plato', 'mesa']
@@ -23,27 +23,61 @@ def main():
     # Choice level
     ts(0.5)
     level = level_choice(name)
-    print(level)
+
+    if level == 1:
+        level = EASY
+    elif level == 2:
+        level = MEDIUM
+    else:
+        level = HARD
 
     # Start game
+    hangman(LIVES, PART_BODY, level, name)
 
 
 # Functions
 
+# Func game
+
+def hangman(live, body, list, name):
+    # Random choice word in list
+    word = ch(list)
+    
+    print('¡El juego va a comenzar!\n En...')
+    
+    # Counter 3 2 1 ...
+    counter()
+    
+    # Start
+    while True:
+            
+        
+            
+    
+
+
 # Level choice
+
+
 def level_choice(name):
     while True:
         level = input(f'''\n¡{name}! Escribe el numero del nivel que quieres jugar (Sin el punto)...
                       1. Facil
                       2. Medio
-                      3. Dificil\n''')
+                      3. Dificil\n''').strip()
         return int(level) if 0 < int(level) <= 3 else print('Por favor escribe un numero valido (Entre 1 - 3).')
-
-# Func game
 
 # Func cipher word
 
 # Func correct word
+
+# Func counter
+
+
+def counter():
+    for i in range(3, 0, -1):
+        print(f'{i}...')
+        ts(0.5)
 
 
 main()
